8a9470d6812b83a4395b2dd61645bf3bed8f889c
Harrison-Blair <hjblair@me.com>
2026-01-16 10:59:39 -0500

    follow LangChain Docs RAG Agent tutorial

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # langchain-rag
 RAG agent based on the [LangChain tutorial](https://docs.langchain.com/oss/python/langchain/rag) documentation.
+
+# Setup
+## Ollama Model
+Pull the model
+```
+ollama pull qwen3:1.7b
+```
+
+Pull embeddings
+```
+ollama pull nomic-embed-text
+```
+
+## `.env` File
+Copy `.env.example`, name it `.env`. Fill out the empty API keys.
+
+## Virtual Enviornment
+Create virtual enviornment
+```
+py -3.13 -m venv .venv
+```
+
+Activate virtual enviornment
+```
+.\.venv\Scripts\activate
+```
+
+Install Requirements
+```
+pip install -r requirements.txt
+```
---
+++ example.py
@@ -0,0 +1,78 @@
+import bs4
+from langchain_ollama import (
+    ChatOllama,
+    OllamaEmbeddings,
+)
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_core.vectorstores import InMemoryVectorStore
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.tools import tool
+from langchain.agents import create_agent
+
+# Load Model
+llm = ChatOllama(
+    model="qwen3:1.7b",
+    temperature=0,
+)
+
+# Load Embeddings
+embeddings = OllamaEmbeddings(model="nomic-embed-text")
+
+# Load Web Page with BeautifulSoup Strainer
+bs4_strainer = bs4.SoupStrainer(class_=("post-title", "post-header", "post-content"))
+loader = WebBaseLoader(
+    web_paths=("https://lilianweng.github.io/posts/2023-06-23-agent/",),
+    bs_kwargs={"parse_only": bs4_strainer},
+)
+docs = loader.load()
+
+assert len(docs) == 1
+print(f"Total characters: {len(docs[0].page_content)}")
+print(docs[0].page_content[:500])
+
+# Split Documents (to fit in context window)
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000,  # chunk size (characters)
+    chunk_overlap=200,  # chunk overlap (characters)
+    add_start_index=True,  # track index in original document
+)
+all_splits = text_splitter.split_documents(docs)
+
+print(f"Split blog post into {len(all_splits)} sub-documents.")
+
+# Add splits to vector store
+vector_store = InMemoryVectorStore.from_documents(
+    all_splits,
+    embeddings
+)
+
+# Create tool for agent to use
+@tool(response_format="content_and_artifact")
+def retrieve_context(query: str):
+    """Retrieve information to help answer a query."""
+    retrieved_docs = vector_store.similarity_search(query, k=2)
+    serialized = "\n\n".join(
+        (f"Source: {doc.metadata}\nContent: {doc.page_content}")
+        for doc in retrieved_docs
+    )
+    return serialized, retrieved_docs
+
+# Create agent with tool
+tools = [retrieve_context]
+prompt = (
+    "You have access to a tool that retrieves context from a blog post. "
+    "Use the tool to help answer user queries."
+)
+agent = create_agent(llm, tools, system_prompt=prompt)
+
+# Run agent with a query
+query = (
+    "What is the standard method for Task Decomposition?\n\n"
+    "Once you get the answer, look up common extensions of that method."
+)
+
+for event in agent.stream(
+    {"messages": [{"role": "user", "content": query}]},
+    stream_mode="values",
+):
+    event["messages"][-1].pretty_print()
