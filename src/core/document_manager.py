00daa28d9aad836115d90a52a6d803a5c48d98f0
Harrison-Blair <hjblair@me.com>
2026-01-25 17:25:06 -0500

    refactor document manager to improve document retrieval and loading

--- src/core/document_manager.py
+++ src/core/document_manager.py
@@ -1,6 +1,7 @@
 """
 Document manager for handling file i/o and document operations.
 """
+from langchain_community.document_loaders import PyMuPDFLoader
 
 from src.core.config_manager import config_manager
 
@@ -13,13 +14,25 @@ class DocumentManager():
     def get_document_list(self) -> list:
         """Retrieve list of documents in the managed directory."""
         import os
-        documents = []
+        document_names = []
         for root, _, files in os.walk(self.directory):
             for file in files:
-                documents.append(os.path.join(root, file))
-        return documents
+                document_names.append(os.path.join(root, file))
+        return document_names
     
     def get_documents(self):
-        pass
+        """Load and return documents from the managed directory."""
+        documents = []
+        for doc in self.get_document_list():
+            loader = PyMuPDFLoader(doc)
+            documents.extend(loader.load())
+        return documents
+    
+    def get_documents_with_uuids(self):
+        """Load documents and assign a unique ID to each."""
+        from uuid import uuid4
+        documents = self.get_documents()
+        uuids = [str(uuid4()) for _ in range(len(documents))]
+        return uuids, documents
 
 document_manager = DocumentManager()
